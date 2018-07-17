9621e90b3b426617845789592df199abd03f317f
Blazingcoder21 <41324630+Blazingcoder21@users.noreply.github.com>
2018-07-17 11:15:01 +0100

    Add files via upload

---
+++ Second_Keyboard_Program.py
@@ -0,0 +1,11 @@
+print ("This is a program to accept Name, Age and Address and display it on the screen")
+myName = input ("Please Enter your Name: ")
+myAge = input ("Please Enter your Age: ")
+myAddress_houseNo = input ("Please Enter your House Number: ")
+myAddress_Street = input ("Please Enter your street Name: ")
+myAddress_postcode = input ("Please Enter your Post Code: ")
+
+print ('Hello, my name is', myName,' ,I am', myAge,' years old, and I live at', myAddress_houseNo,', ',myAddress_Street,', ',myAddress_postcode)
+
+
+
